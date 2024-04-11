06bb0a1304c73e69d1968a08c9cba07cc91457d9
OIgnacioA <sehent@hotmail.com>
2024-04-11 14:33:27 -0300

    intento 2 -jenkinsfile para W con el hello world modificado y test tambien

--- index.test.js
+++ index.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('./index');
 
 describe('GET /', () => {
-    it('responds with hello world message in JSON format', async () => {
+    it('responds with a message in JSON format', async () => {
         const response = await request(app).get('/');
         expect(response.statusCode).toBe(200);
-        expect(response.body).toEqual({ message: 'Hello, world!' });
+        expect(response.body).toHaveProperty('message');
     });
 });
