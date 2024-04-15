6a6bb568144a91a53cc7621746680d2006442a4c
OIgnacioA <sehent@hotmail.com>
2024-04-15 13:20:59 -0300

    hola mundo33, webhook apuntando a mi jenkisn local, agregago plugin a jenkins

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello, world 3!' });
+    res.json({ message: 'Hello, world 33!' });
 });
 
 const port = process.env.PORT || 3000;
